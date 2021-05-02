50cd1d91a787c09ffa62f9045bc9f36ae1e318af
Aleksandar Stojanovic <sale4319@gmail.com>
2021-05-02 19:06:08 +0200

    connected checout button, small refactoring fixes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,28 +9,27 @@ import useStyles from './styles';
 const Navbar = ({ totalItems }) => {
     const classes = useStyles();
     const location = useLocation();
+    const isUrlPathRoot = location.pathname === '/';
 
     return (
-        <>
-            <AppBar position="fixed" className={classes.appBar} color="inherit">
-                <Toolbar>
-                    <Typography component={Link} to="/" variant="h6" className={classes.title} color="inherit">
-                        <img src={logo} alt="Bike Store" height="25px" className={classes.image} />
+        <AppBar position="fixed" className={classes.appBar} color="inherit">
+            <Toolbar>
+                <Typography component={Link} to="/" variant="h6" className={classes.title} color="inherit">
+                    <img src={logo} alt="Bike Store" height="25px" className={classes.image} />
                         Bike Store
                     </Typography>
-                    <div className={classes.grow} />
-                    {location.pathname === '/' && (
-                        <div className={classes.button}>
-                            <IconButton component={Link} to="/cart" aria-label="Show cart items" color="inherit">
-                                <Badge badgeContent={totalItems} color="secondary">
-                                    <ShoppingCart />
-                                </Badge>
-                            </IconButton>
-                        </div>
-                    )}
-                </Toolbar>
-            </AppBar>
-        </>
+                <div className={classes.grow} />
+                {isUrlPathRoot && (
+                    <div className={classes.button}>
+                        <IconButton component={Link} to="/cart" aria-label="Show cart items" color="inherit">
+                            <Badge badgeContent={totalItems} color="secondary">
+                                <ShoppingCart />
+                            </Badge>
+                        </IconButton>
+                    </div>
+                )}
+            </Toolbar>
+        </AppBar>
     )
 }
 
