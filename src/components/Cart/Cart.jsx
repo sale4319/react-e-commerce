2d582c1d8eca2b16a4c679dfe5e7bbb8ff9efa2e
Aleksandar Stojanovic <sale4319@gmail.com>
2021-05-04 21:51:41 +0200

    optimized mobile view

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -30,7 +30,7 @@ const Cart = ({ cart, onUpdateCartQty, onRemoveFromCart, onEmptyCart }) => {
             <div className={classes.cardDetails}>
                 <Typography variant="h4" className={classes.cardText}>Subtotal: {cart.subtotal.formatted_with_symbol}</Typography>
                 <div>
-                    <Button className={classes.emptyButton} size="large" type="button" variant="contained" startIcon={<DeleteIcon />} onClick={onEmptyCart}>Empty Cart</Button>
+                    <Button className={classes.emptyButton} size="large" type="button" variant="contained" startIcon={<DeleteIcon />} onClick={onEmptyCart}>Empty</Button>
                     <Button component={Link} to="/checkout" className={classes.checkoutButton} size="large" type="button" variant="contained" color="primary" endIcon={<NavigateNext />}>Checkout</Button>
                 </div>
             </div>
